b32d97e17f2bab5521864150cf010c24af4c3d94
ArifulIslam99 <afridi2000rampura@gmail.com>
2025-05-12 22:05:19 +0600

    B5-Assignment-1

---
+++ assignment.ts
@@ -0,0 +1,23 @@
+function formatString(input: string, toUpper?: boolean): string {
+    if (toUpper == false) {
+        return input.toLowerCase();
+    } else {
+        return input.toUpperCase();;
+    } 
+}
+
+// const result1 = formatString("hello world", true); 
+// console.log(result1);
+
+function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
+    return items.filter(item => item.rating >= 4);
+}
+
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 }
+];
+
+// const result2 = filterByRating(books); 
+// console.log(result2); 
